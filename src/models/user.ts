951348b3bbfdbece7b6c9cf01f1c2d9a13152ed9
Sangat Das <sangatdas5@gmail.com>
2023-12-08 22:31:05 +0530

    Added user model

---
+++ src/models/user.ts
@@ -0,0 +1,32 @@
+/** Required Node Modules */
+import { model, Schema, Model, Document } from 'mongoose';
+
+/** Required App Modules */
+
+/** Required Interface */
+export interface IUser extends Document {
+    email:string;
+    address: string,
+    isLender: boolean
+}
+
+const UserSchema: Schema = new Schema({
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    address: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    isLender: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+const User :Model<IUser> | any = model("User", UserSchema)
+export default User;
